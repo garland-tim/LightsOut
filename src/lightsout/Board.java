257f5acde5bf6cab0a700e669cad8af79443a6cb
cgood92 <clintgoodman27@gmail.com>
2013-10-21 12:27:20 -0600

    First time

--- src/lightsout/Board.java
+++ src/lightsout/Board.java
@@ -13,53 +13,42 @@ public class Board {
     int boardHeight = 5;
     int boardWidth = 5;
     String[] topLabels = {"A","B","C","D","E"};
-    String[] sideLables = {"1","2","3","4","5"};
+    String[] sideLabels = {"1","2","3","4","5"};
     
     public Board(){
     }
     
     public void displayBoard(){
         //Top Row
+        String topRow = "   ";
+        for(int i=0; i < this.topLabels.length; i++)
+        {
+            topRow += " "+ topLabels[i] +" ";
+        }
+        System.out.println(topRow);
+        
         for(int h=0; h < this.boardHeight; h++)
         {
-            String boardLines = "=";
+            String boardLines = "   =";
             for(int w=0; w < this.boardWidth; w++)
             {
                 boardLines += "===";
             }
             System.out.println(boardLines);
-            boardLines = "|";
+            boardLines = " "+ this.sideLabels[h] +" |";
             for(int w=0; w < this.boardWidth; w++)
             {
                 boardLines += "  |";
             }
             System.out.println(boardLines);
         }
-        String boardLines = "=";
+        
+        //Bottom =========
+        String boardLines = "   =";
         for(int w=0; w < this.boardWidth; w++)
           {
               boardLines += "===";
           }
         System.out.println(boardLines);
-    }
-    
-    public void displayDefaults() {
-        System.out.println("Default Height: " + this.boardHeight);
-        System.out.println("Default Width: " + this.boardWidth);
-    }
-    
-    public void makeBoard(int height, int width){
-        for(int x = 0; x < boardWidth; x++)
-        {
-            System.out.println("\t" + topLabels[x] + "\t");
-        }
-        System.out.println("\n");
-        if(this.boardHeight > 0 && this.boardWidth > 0)
-        {
-            for (int y = 0; y <= this.boardWidth; y++)
-            {
-                System.out.println("---");
-            }
-        }
     }
 }
