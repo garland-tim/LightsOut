455fcc1756f04735e0fc32c60415a2cb01e2644c
cgood92 <clintgoodman27@gmail.com>
2013-11-14 22:27:32 -0700

    First time

--- src/lightsout/Player.java
+++ src/lightsout/Player.java
@@ -13,8 +13,8 @@ import java.util.Scanner;
 public class Player {
     private String name;
 
-    public Player(String name) {
-        this.name = name;
+    public Player() {
+        
     }
 
     public String getName() {
