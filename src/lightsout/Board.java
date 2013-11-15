cd6b667f8df723eb371304e4d74d3fb897666566
cgood92 <clintgoodman27@gmail.com>
2013-11-14 22:37:32 -0700

    First time

--- src/lightsout/Board.java
+++ src/lightsout/Board.java
@@ -189,4 +189,50 @@ public class Board implements Serializable {
         rowInt = Integer.parseInt(row);
         return rowInt-1;
     }   
+    
+public class Light implements Serializable {
+    Board board;
+    private char onSymbol = (char)88;
+    private char offSymbol = (char)32;
+    
+    public Light(Board board){
+        this.board = board;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    public char getOnSymbol() {
+        return onSymbol;
+    }
+
+    public void setOnSymbol(char onSymbol) {
+        this.onSymbol = onSymbol;
+    }
+
+    public char getOffSymbol() {
+        return offSymbol;
+    }
+
+    public void setOffSymbol(char offSymbol) {
+        this.offSymbol = offSymbol;
+    }
+    
+    public void changeLight(int columnInt, int rowInt){
+        if(this.board.boardArray[rowInt][columnInt] == 1)
+        {
+            this.board.boardArray[rowInt][columnInt] = 0;
+        }
+        else{
+            this.board.boardArray[rowInt][columnInt] = 1;
+        }
+    }
+
+}
+
 }
