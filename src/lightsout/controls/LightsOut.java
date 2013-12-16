b62800dedd6266547d16fa183e86148f54ee8132
cgood92 <clintgoodman27@gmail.com>
2013-12-16 11:10:01 -0700

    First time

--- src/lightsout/controls/LightsOut.java
+++ src/lightsout/controls/LightsOut.java
@@ -18,9 +18,7 @@ public class LightsOut {
     public static MainFrame mainFrame = null;
     
     public static void main(String[] args) {
-        try{
-            System.out.println("Goal of the game: Turn off all the lights on the board!\nClick on a space, and that space as well as the spaces above, \nto the left, right, and bottom of it will be inversed.\nContinue to inverse the lights until all lights\nare turned off.\n\n");
-                        
+        try{              
                           /* Create and display the form */
             java.awt.EventQueue.invokeLater(new Runnable() {
                 public void run() {
